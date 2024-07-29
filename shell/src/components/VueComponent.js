c68cc6295de91a6358ecaceb109b79d724303ecd
Konstantin BIFERT <hello@kissu.io>
2024-07-29 04:31:47 +0200

    init

--- shell/src/components/VueComponent.js
+++ shell/src/components/VueComponent.js
@@ -1,11 +1,12 @@
-import { createApp, ref } from 'vue';
+import MicroFrontendVue from "http://localhost:7400/bundle.js";
+import { createApp, ref, h, render } from 'vue';
+console.log('a', MicroFrontendVue)
 
-createApp({
+const Vue = createApp({
   setup() {
     const message = ref('Hello World!')
-
-    return {
-      message
-    }
+    return () => render(MicroFrontendVue.setup())
   }
 }).mount('#app');
+
+export default Vue;
